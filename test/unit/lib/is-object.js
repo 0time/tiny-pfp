1fc4f37641a701bf1cab5ae930f1d38efae259f6
Henry Brown <h@0ti.me>
2020-10-16 17:30:27 -0500

    Remove commented out test code, skip instead of comment out sanity checking test code

--- test/unit/lib/is-object.js
+++ test/unit/lib/is-object.js
@@ -1,4 +1,4 @@
-const { d, nextInt, testRunner, tquire } = deps;
+const { _, d, expect, nextInt, testRunner, tquire } = deps;
 
 const me = __filename;
 
@@ -36,9 +36,8 @@ d(me, () => {
     undefined,
   ];
 
-  /* This bit is useful for sanity checking */
-  /*
-  describe('test assumptions about _.isObject', () => {
+  /* This is useful for sanity checking */
+  describe.skip('test assumptions about _.isObject', () => {
     describe(`${_.isObject}`, () => {
       describe(`${isObject}`, () => {
         positiveTests.forEach(input => {
@@ -57,7 +56,6 @@ d(me, () => {
       });
     });
   });
-  */
 
   const createTestFixturesCreator = (be, expected) => inputSets =>
     inputSets.map(input => ({
@@ -68,66 +66,6 @@ d(me, () => {
       functionToTest: isObject,
       input: () => input,
     }));
-
-  /*
-  const passSymbol = Symbol();
-
-  const tryToString = val => {
-    try {
-      return val.toString();
-    } catch (err) {
-      return 'no-to-string-method-functionality';
-    }
-  };
-
-  const secondaryTestFixturesCreator = pass => inputSets =>
-    inputSets.map(input => ({
-      description: `(${JSON.stringify(input)}) aka (${tryToString(
-        input,
-      )}) (typeof ${typeof input}) should ${
-        pass ? 'succeed' : 'error'
-      } when trying to Object.assign`,
-      expected: passSymbol,
-      functionToTest: () => {
-        const key = nextInt(10, 2).toString();
-        const expected = _.set(_.clone(input), key, 1);
-        let result1 = null;
-        let result2 = null;
-
-        if (pass) {
-          result1 = Object.assign({}, _.clone(input), { [key]: 1 });
-          result2 = Object.assign(_.clone(input), { [key]: 1 });
-
-          expect(result1).to.deep.equal(expected);
-          expect(result2).to.deep.equal(expected);
-        } else {
-          try {
-            result1 = Object.assign({}, _.clone(input), { [key]: 1 });
-          } catch (err) {}
-
-          try {
-            result2 = Object.assign(_.clone(input), { [key]: 1 });
-          } catch (err) {}
-
-          expect(result1).to.deep.equal(input);
-          expect(result2).to.deep.equal(input);
-        }
-
-        return passSymbol;
-      },
-      input,
-    }));
-  const secondaryPositiveTestFixtures = secondaryTestFixturesCreator(true)(
-    positiveTests,
-  );
-  const secondaryNegativeTestFixtures = secondaryTestFixturesCreator(false)(
-    negativeTests,
-  );
-
-  secondaryPositiveTestFixtures
-    .concat(secondaryNegativeTestFixtures)
-    .forEach(testRunner);
-  */
 
   const positiveTestFixtures = createTestFixturesCreator('be', true)(
     positiveTests,
